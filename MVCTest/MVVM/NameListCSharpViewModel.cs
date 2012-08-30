2e866a305cb7e7789a3cc6f8f9c1b0538c14ebfb
NN--- <nn1436401@gmail.com>
2012-08-30 19:26:16 +0300

    C# ViewModel !

---
+++ MVCTest/MVVM/NameListCSharpViewModel.cs
@@ -0,0 +1,43 @@
+﻿using NemerleWeb;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MVCTest
+{
+  [JsModel]
+  partial class NameListCSharpViewModel
+  {
+    List<string> Names { get; set; }
+    string SearchPattern { get; set; }    
+    List<string> FilteredNames
+    {
+      get
+      {
+        if(SearchPattern == null)
+          return Names;
+        else
+        {
+          var result = new List<string>();
+          foreach(var name in Names)
+            if(name.ToUpper().Contains(SearchPattern.ToUpper()))
+              result.Add(name);
+          return result;
+        }
+      }
+    }    
+    
+    public NameListCSharpViewModel()
+    {
+      // TODO: Workaround for C#, because operator= return type of variable, not void as in Nemerle.
+      server.GetNames(l => { Names = l; });
+    }
+    
+    public class Server
+    {
+      public List<string> GetNames()
+      {
+        return Helpers.GetNames().ToList();
+      }
+    }
+  }
+}
