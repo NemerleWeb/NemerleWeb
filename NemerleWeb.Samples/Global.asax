086d553d9fb46982a6a3eb98040b9012d86fc5a6
NN--- <nn1436401@gmail.com>
2012-09-06 13:12:38 +0300

    MVCTest => NemerleWeb.Samples

---
+++ NemerleWeb.Samples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.n" Inherits="NemerleWeb.Samples.MvcApplication" Language="Nemerle" %>
--- NemerleWeb.Samples/MVVM/NameListCSharpViewModel.cs
+++ NemerleWeb.Samples/MVVM/NameListCSharpViewModel.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace MVCTest
+namespace NemerleWeb.Samples
 {
   [JsModel]
   partial class NameListCSharpViewModel
