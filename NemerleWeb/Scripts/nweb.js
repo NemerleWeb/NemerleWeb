aa420875f93c108869889a316d4750004471cb13
NN--- <nn1436401@gmail.com>
2013-02-21 19:28:58 +0200

    Implement Dictionary and Hashtable. Add JsAst.Delete. Add CSharpParser to the build without parsers :)

--- NemerleWeb/Scripts/nweb.js
+++ NemerleWeb/Scripts/nweb.js
@@ -693,6 +693,37 @@ if (!Array.prototype.indexOf) {
     };
 }
 
+// Implement Object.keys for browser which doens't support
+Object.keys = Object.keys || (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
+        DontEnums = [ 
+            'toString', 'toLocaleString', 'valueOf', 'hasOwnProperty',
+            'isPrototypeOf', 'propertyIsEnumerable', 'constructor'
+        ],
+        DontEnumsLength = DontEnums.length;
+
+    return function (o) {
+        if (typeof o != "object" && typeof o != "function" || o === null)
+            throw new TypeError("Object.keys called on a non-object");
+
+        var result = [];
+        for (var name in o) {
+            if (hasOwnProperty.call(o, name))
+                result.push(name);
+        }
+
+        if (hasDontEnumBug) {
+            for (var i = 0; i < DontEnumsLength; i++) {
+                if (hasOwnProperty.call(o, DontEnums[i]))
+                    result.push(DontEnums[i]);
+            }   
+        }
+
+        return result;
+    };
+})();
+
 String.prototype.getEnumerator = function () {
     this.__enumeratorIndex = -1;
     this.Current = null;
