afbab7e78f98628f796c7f98075114d89e083cf4
Heather Mason <heather@cynede.net>
2013-02-28 14:10:01 +0400

    4.0 project files + bat files (to run 4.0 build fill Nemerle/Net-4.0 folder)

---
+++ Build-4.5.cmd
@@ -0,0 +1,9 @@
+@echo off
+if %PROCESSOR_ARCHITECTURE%==x86 (
+	set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+) else (
+	set MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+)
+set EnableNuGetPackageRestore=true
+
+%MSBuild% NemerleWeb.sln /tv:4.0 /p:TargetFrameworkVersion=v4.5 /t:Build /p:Configuration=Release
