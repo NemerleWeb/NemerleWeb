6a498b8cadc9e9894e89d4045c70685f402541b7
ionoy <mihhail.maslakov@gmail.com>
2013-07-31 19:01:39 +0300

    added standalone installer

--- create-installer.ps1
+++ create-installer.ps1
@@ -1,4 +1,27 @@
+if(test-path .\NemerleWeb.ProjectTemplate\bin) {
+    Remove-Item .\NemerleWeb.ProjectTemplate\bin -Force -Recurse
+}
 
+if(test-path .\NemerleWeb.ProjectTemplate\obj) {
+    Remove-Item .\NemerleWeb.ProjectTemplate\obj -Force -Recurse
+}
 
-.\7z a -tzip ProjectTemplate.zip NemerleWeb.ProjectTemplate
-.\7z a -tzip ItemTemplate.zip NemerleWeb.ItemTemplate
+if(test-path .\NemerleWeb.ItemTemplate\bin) {
+    Remove-Item .\NemerleWeb.ItemTemplate\bin -Force -Recurse
+}
+
+if(test-path .\NemerleWeb.ItemTemplate\obj) {
+    Remove-Item .\NemerleWeb.ItemTemplate\obj -Force -Recurse
+}
+
+.\7z a -tzip PSTools\ProjectTemplate.zip .\NemerleWeb.ProjectTemplate
+.\7z a -tzip PSTools\ItemTemplate.zip .\NemerleWeb.ItemTemplate\Page.n
+
+if(test-path .\PSTools\Installer\AllTools.zip) {
+    Remove-Item .\PSTools\Installer\AllTools.zip
+}
+
+.\7z a -tzip PSTools\Installer\AllTools.zip .\PSTools\*
+
+copy .\7z.exe .\PSTools\Installer
+copy .\PSTools\standalone-install.ps1 .\PSTools\Installer
