d90920420629a79978dd348a196adefe42a09f2e
ionoy <mihhail.maslakov@gmail.com>
2013-08-02 17:36:50 +0300

    standalone installer #1

--- PSTools/standalone-install.ps1
+++ PSTools/standalone-install.ps1
@@ -16,9 +16,9 @@ $webClient.DownloadFile('http://www.nemerleweb.com/installer/AllTools.zip', (joi
 .\7z e -tzip ItemTemplate.zip -y | out-null
 .\7z x -tzip ProjectTemplate.zip -y | out-null
 
-#.\dl-install-net45.ps1
-#.\check-nemerle.ps1
-#.\install-nweb.ps1
+.\dl-install-net45.ps1
+.\check-nemerle.ps1
+.\install-nweb.ps1
 
 pop-location
 
