a61561226c11a4fe0ae9e3c0e6477c8f7dfdd4dd
NN <nn1436401@gmail.com>
2013-10-19 18:28:30 +0300

    Require parameters in deploy.

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
-param([String]$from, [String]$to)
+param([String]$from=$(throw "From required"), [String]$to=$(throw "To required"))
 
 Copy-Item $from\bin         $to -Recurse -Force
 Copy-Item $from\Scripts     $to -Recurse -Force
