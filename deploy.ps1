6be5ebc20e2c530b0f2bcbacaf3c0f094191c983
NN <nn1436401@gmail.com>
2013-10-20 18:44:26 +0300

    Deploy App_Data

--- deploy.ps1
+++ deploy.ps1
@@ -4,5 +4,8 @@ Copy-Item $from\bin         $to -Recurse -Force
 Copy-Item $from\Scripts     $to -Recurse -Force
 Copy-Item $from\Content     $to -Recurse -Force
 Copy-Item $from\Views       $to -Recurse -Force
+if (Test-Path $from\App_Data) {
+	Copy-Item $from\App_Data    $to -Recurse -Force
+}
 Copy-Item $from\Web.config  $to -Force
 Copy-Item $from\Global.asax $to -Force
