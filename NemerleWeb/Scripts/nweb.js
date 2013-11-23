267588bb1236d1560c3b6b40cd29591f4525ec64
NN <nn1436401@gmail.com>
2013-11-23 18:57:18 +0200

    Use _NW$_self

--- NemerleWeb/Scripts/nweb.js
+++ NemerleWeb/Scripts/nweb.js
@@ -471,7 +471,7 @@ var nweb = {
 
         if (!cachedFunc) {
           //var newFunc = eval("(function(model, loopStack) { return " + parsedExpr + "; } )");
-          var newFunc = new Function('model', 'loopStack', "var self = model; return " + parsedExpr + ";");
+          var newFunc = new Function('model', 'loopStack', "var _NW$_self = model; return " + parsedExpr + ";");
           nweb.parsedValueCache[parsedExpr] = newFunc;
           val = newFunc(model, loopStack);
         } else {
