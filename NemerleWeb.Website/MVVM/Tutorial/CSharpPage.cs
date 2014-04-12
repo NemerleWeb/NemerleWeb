a49c4914f68b1d9a6c2065b955209609d93eeaac
ionoy <mihhail.maslakov@gmail.com>
2014-04-12 12:52:46 +0300

    added c# tutorial sample

---
+++ NemerleWeb.Website/MVVM/Tutorial/CSharpPage.cs
@@ -0,0 +1,32 @@
+﻿using Nemerle;
+using Nemerle.Collections;
+using Nemerle.Text;
+using Nemerle.Utility;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NemerleWeb;
+
+namespace NemerleWeb.Website
+{
+  [Unit]
+  public class CSharpPage
+  {
+    public readonly string Name;
+
+    public CSharpPage()
+    {
+        Name = "C#";
+    }
+
+    [Html]
+    public string View()
+    {
+       return @"
+         <input value=""$Name"" />
+         <div>Hello, $Name</div>
+       ";
+    }
+  }
+}
