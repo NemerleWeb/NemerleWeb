feab0165f75c56d456183c64d1917e7c27054db2
NN <nn1436401@gmail.com>
2014-04-18 10:47:40 +0300

    Add C# tests.

---
+++ NemerleWeb.Website/MVVM/Tests/CSClasses.cs
@@ -0,0 +1,29 @@
+﻿using NemerleWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace NemerleWeb.Website.Tests
+{
+	[Unit]
+	public class FieldsInitializationCS
+	{
+		public int Field = 1;
+		public readonly int ReadonlyField = 2;
+	}
+
+	[Unit]
+	public class ConstructorFieldsInitializationCS
+	{
+		public int Field;
+		public readonly int ReadonlyField;
+
+		public ConstructorFieldsInitializationCS()
+		{
+			this.Field = 1;
+			this.ReadonlyField = 2;
+		}
+	}
+}
