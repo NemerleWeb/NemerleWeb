3ce2528df637bbb489d75ea366e8ad8d03f33dae
NN <nn1436401@gmail.com>
2014-04-24 12:56:25 +0300

    Add C# file testing.

---
+++ Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using Nemerle;
+using NemerleWeb;
+
+namespace Test
+{
+  /// <summary>
+  /// Description of Test.
+  /// </summary>
+  [Unit]
+  class FieldInitialization
+  {
+	  public int Field = 1;
+  }
+}
